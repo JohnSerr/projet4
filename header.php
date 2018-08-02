b24cb2e9884351a8b05f2a15a03d220ea7c2843a
Fumble <serrier.jonathan@gmail.com>
2018-08-02 16:45:56 +0200

    code menu.php + modif header.php

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <header>
 	<div id="header">
 	
-		<div id="logo"></div>
-		
+		<div id="logo1"></div>
+
     	<h1> Blog de Jean ROCHEFORT, la fantasy c'est sont fort ! </h1>
+
+    	<div id="logo2"></div>
 
     </div>	
 
---
+++ menu.php
@@ -0,0 +1,9 @@
+<nav>
+	<ul>
+		<li>Accueil</li>
+		<li>Billets</li>
+		<li>Contact</li>
+	</ul>
+
+
+</nav>
