32d1133d08cf04646cce86ef46577119c6f81a22
Fumble <serrier.jonathan@gmail.com>
2018-08-02 20:27:05 +0200

    update css indexView

--- indexView.php
+++ indexView.php
@@ -6,25 +6,29 @@
 	  include("menu.php");
 ?>
 
+	<div id="bandeau"></div>
+
 	<div id="intro">
-		
-		<h2>Bienvenue sur mon Blog !</h2>
-		
-		<p>Je m'appelle Jean Forteroche.!</p>
-		<p>Je m'apprête à vous faire partager mon prochain livre, pas à pas via ce site.</p>
-		<p><em>Billet simple pour l'Alaska</em> est son titre</p>
-		<p>Veinard que vous êtes ! Profitez en bien !</p>
 	
+		<div id="bienvenue">
+			<h2>Bienvenue sur mon Blog !</h2>
+		
+			<p>Je m'appelle Jean Forteroche.!</p>
+			<p>Je m'apprête à vous faire partager mon prochain livre, pas à pas via ce site.</p>
+			<p><em>Billet simple pour l'Alaska</em> est son titre.</p>
+			<p>Veinard que vous êtes ! Profitez en bien !</p>
+			<p id="signature">Jean FORTEROCHE</p>
+		</div>
+
+
+		<div id="livre">
+			<img src="livre.jpg" alt="livre">
+		</div>
+
 	</div>
-
-
-	<div id="livre">
-		<img src="livre.jpg" alt="livre">
-	</div>
-
 
 	<div id="lastpost">
-		
+		place réservé pour le dernier post écrit par Jean Forteroch
 	</div>
 
 <?php include("footer.php"); ?>
--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <nav>
 	<ul>
 		<li><a href="indexView.php">Accueil</a></li>
-		<li><a href="postView.php">Billets</a></li>
+		<li><a href="postView.php">Chapitres</a></li>
 		<li><a href="contact.php">Contact</a></li>
 	</ul>
 
